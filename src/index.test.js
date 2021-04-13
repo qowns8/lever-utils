0588152ff56e4c310f547bdfcdceae9c7f938549
keating <mgsong@madup.com>
2021-04-13 10:18:44 +0900

    reformat

--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,15 @@
-import {getSearchParams, randomStr} from './index'
+import { getSearchParams, randomStr } from './index'
 
 describe('utils/lib', () => {
   test('getSearchParam', () => {
-    expect(getSearchParams('?aa=11')).toEqual({aa: '11'})
-    expect(getSearchParams('?aa=11&bb=22')).toEqual({aa: '11', bb: '22'})
+    expect(getSearchParams('?aa=11')).toEqual({ aa: '11' })
+    expect(getSearchParams('?aa=11&bb=22')).toEqual({ aa: '11', bb: '22' })
     expect(getSearchParams('')).toEqual({})
-    expect(getSearchParams('https://biz.lever.me?a=1&b=2&c=3')).toEqual({a:'1', b:'2', c:'3'})
+    expect(getSearchParams('https://biz.lever.me?a=1&b=2&c=3')).toEqual({
+      a: '1',
+      b: '2',
+      c: '3',
+    })
   })
   test('randomStr', () => {
     expect(randomStr() !== randomStr()).toEqual(true)
--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,5 @@ export const getSearchParams = (url: string): IPlainObject => {
   return params
 }
 
-export const randomStr = () => (((1 + Math.random()) * 0x10000) | 0).toString(16)
+export const randomStr = () =>
+  (((1 + Math.random()) * 0x10000) | 0).toString(16)
