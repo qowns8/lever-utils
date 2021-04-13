bc04e4cb99430268cbf12e54b0be9a808e5d59de
keating <mgsong@madup.com>
2021-04-13 10:09:26 +0900

    setting of eslint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-export const getSearchParams = (url: string) => {
-  const params: any = {}
+import {IPlainObject} from "./types";
+
+export const getSearchParams = (url: string): IPlainObject => {
+  const params: IPlainObject = {}
   const idx = url.indexOf('?') + 1
   const fromIdx = url.slice(idx)
   // @ts-ignore
-  fromIdx.replace(/([^(?|#)=&]+)(=([^&]*))?/g, ($0, $1, $2, $3) => {
+  fromIdx.replace(/([^(?|#)=&]+)(=([^&]*))?/g, ($0, $1 , $2, $3) => {
     params[$1] = decodeURIComponent($3)
   })
   return params
