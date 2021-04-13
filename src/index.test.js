d57b9abc683c9df7a921706d2e2fc63d18f3509f
keating <mgsong@madup.com>
2021-04-13 10:17:13 +0900

    add testcase of getSearchParam

--- src/index.test.js
+++ src/index.test.js
@@ -5,6 +5,7 @@ describe('utils/lib', () => {
     expect(getSearchParams('?aa=11')).toEqual({aa: '11'})
     expect(getSearchParams('?aa=11&bb=22')).toEqual({aa: '11', bb: '22'})
     expect(getSearchParams('')).toEqual({})
+    expect(getSearchParams('https://biz.lever.me?a=1&b=2&c=3')).toEqual({a:'1', b:'2', c:'3'})
   })
   test('randomStr', () => {
     expect(randomStr() !== randomStr()).toEqual(true)
