6d1848abc6ae6cff325c89cffe0f228a55ec843c
keating <mgsong@madup.com>
2021-05-13 17:38:37 +0900

    feat: add validateBiznum

--- src/index.test.ts
+++ src/index.test.ts
@@ -4,9 +4,10 @@ import {
   serializeSearchParams,
   isNotNil,
   isNil,
+  validateBiznum,
 } from './index'
 
-describe('utils/lib', () => {
+describe('Lever utils', () => {
   test('parseSearchParams', () => {
     expect(parseSearchParams('?aa=11')).toEqual({ aa: '11' })
     expect(parseSearchParams('?aa=11&bb=22')).toEqual({ aa: '11', bb: '22' })
@@ -19,6 +20,7 @@ describe('utils/lib', () => {
     const str: string = parseSearchParams('?aa=11&bb=22').aa // check type of return value
     expect(str).toEqual('11')
   })
+
   test('serializeSearchParams', () => {
     expect(serializeSearchParams({ a: 1, b: 2 })).toEqual('a=1&b=2')
     expect(serializeSearchParams({ a: 1, b: undefined })).toEqual('a=1')
@@ -26,10 +28,12 @@ describe('utils/lib', () => {
     expect(serializeSearchParams({})).toEqual('')
     expect(serializeSearchParams()).toEqual('')
   })
+
   test('randomStr', () => {
     expect(randomStr() !== randomStr()).toEqual(true)
     expect(randomStr().length).toEqual(5)
   })
+
   test('isNil', () => {
     expect(isNil(null)).toEqual(true)
     expect(isNil(undefined)).toEqual(true)
@@ -37,11 +41,22 @@ describe('utils/lib', () => {
     expect(isNil('')).toEqual(false)
     expect(isNil()).toEqual(true)
   })
+
   test('isNotNil', () => {
     expect(isNotNil(null)).toEqual(false)
     expect(isNotNil(undefined)).toEqual(false)
     expect(isNotNil(0)).toEqual(true)
     expect(isNotNil('')).toEqual(true)
     expect(isNotNil()).toEqual(false)
+  })
+
+  test('validateBiznum.', () => {
+    expect(validateBiznum('')).toEqual(false)
+    expect(validateBiznum()).toEqual(false)
+    expect(validateBiznum('2341241242')).toEqual(false) // 아무거나
+    expect(validateBiznum('1198706634')).toEqual(true) // 매드업
+    expect(validateBiznum('2208162517')).toEqual(true) // 네이버
+    expect(validateBiznum('1248100998')).toEqual(true) // 삼성전자
+    expect(validateBiznum('1198702720')).toEqual(true) // 주빅밴드앤코
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -33,3 +33,30 @@ export const serializeSearchParams = (obj: IPureObject = {}): string =>
 
 export const randomStr = (): string =>
   (((1 + Math.random()) * 0x10000) | 0).toString(16)
+
+export const validateBiznum = (biznum?: string): boolean => {
+  // https://myhappyman.tistory.com/129
+  if (!biznum) {
+    return false
+  }
+  const numberMap = biznum
+    .replace(/-/gi, '')
+    .split('')
+    .map(function (d) {
+      return parseInt(d, 10)
+    })
+
+  if (numberMap.length === 10) {
+    const keyArr = [1, 3, 7, 1, 3, 7, 1, 3, 5]
+    let chk = 0
+
+    keyArr.forEach(function (d, i) {
+      chk += d * numberMap[i]
+    })
+
+    chk += parseInt(String((keyArr[8] * numberMap[8]) / 10), 10)
+    return Math.floor(numberMap[9]) === (10 - (chk % 10)) % 10
+  }
+
+  return false
+}
