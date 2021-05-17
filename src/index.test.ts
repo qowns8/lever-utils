ddaf8ae9293520795c0a069e4eb1ccdc79a6d6f0
keating <mgsong@madup.com>
2021-05-17 14:06:01 +0900

    add testcase of validateBiznum
    https://madup.atlassian.net/browse/AD-1299

--- src/index.test.ts
+++ src/index.test.ts
@@ -59,6 +59,7 @@ describe('Lever utils', () => {
     expect(validateBiznum('1198706634')).toEqual(true) // 매드업
     expect(validateBiznum('2208162517')).toEqual(true) // 네이버
     expect(validateBiznum('1248100998')).toEqual(true) // 삼성전자
-    expect(validateBiznum('1198702720')).toEqual(true) // 주빅밴드앤코
+    expect(validateBiznum('1198702720')).toEqual(true)
+    expect(validateBiznum('4528700430')).toEqual(true)
   })
 })
