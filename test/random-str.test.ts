83f055cf6c9ae605f3fcad91ac686133afcfd789
keating <mgsong@madup.com>
2021-06-18 11:41:27 +0900

    update desc

--- test/random-str.test.ts
+++ test/random-str.test.ts
@@ -1,8 +1,7 @@
 import { randomStr } from "../src";
 
 describe('randomStr', () => {
-
-  test('randomStr', () => {
+  test('5개의 문자로 구성된 랜덤 문자열을 리턴', () => {
     expect(randomStr() !== randomStr()).toEqual(true)
     expect(randomStr().length).toEqual(5)
   })
