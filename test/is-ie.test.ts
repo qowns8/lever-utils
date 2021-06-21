ca6e3058da4ca4123f152159bb0f1ce2f0e3048a
keating <mgsong@madup.com>
2021-06-21 15:59:43 +0900

    add testcase of isIE

--- test/is-ie.test.ts
+++ test/is-ie.test.ts
@@ -30,12 +30,13 @@ describe('isIE', () => {
       }),
     ).toEqual(false)
 
-    // expect(
-    //   isIE({
-    //     userAgent:
-    //       'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
-    //   }),
-    // ).toEqual(true)
+    expect(
+      isIE({
+        appName: 'Netscape',
+        userAgent:
+          'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+      }),
+    ).toEqual(true)
 
     expect(
       isIE({
@@ -44,12 +45,13 @@ describe('isIE', () => {
       }),
     ).toEqual(true)
 
-    // expect(
-    //   isIE({
-    //     userAgent:
-    //       'Mozilla/5.0 (Mobile; Windows Phone 8.1; Android 4.0; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 520) like iPhone OS 7_0_3 Mac OS X AppleWebKit/537 (KHTML, like Gecko) Mobile Safari/537',
-    //   }),
-    // ).toEqual(true)
+    expect(
+      isIE({
+        appName: 'Netscape',
+        userAgent:
+          'Mozilla/5.0 (Mobile; Windows Phone 8.1; Android 4.0; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; NOKIA; Lumia 520) like iPhone OS 7_0_3 Mac OS X AppleWebKit/537 (KHTML, like Gecko) Mobile Safari/537',
+      }),
+    ).toEqual(true)
 
     expect(
       isIE({
