13e108bf6ca3bbf4749053d7c682ad0c4f0e6811
keating <mgsong@madup.com>
2021-06-24 10:53:03 +0900

    쿠키의 이름이 중복될 경우 테스트케이스 추가

--- test/get-cookie.test.ts
+++ test/get-cookie.test.ts
@@ -1,9 +1,13 @@
-import { getCookie } from "../src";
+import { getCookie } from '../src'
 
 describe('getCookie', () => {
   test('name 에 해당하는 쿠키값을 리턴', () => {
     const cookie = 'aa=11; bb=22'
     expect(getCookie('aa', cookie)).toEqual('11')
     expect(getCookie('bb', cookie)).toEqual('22')
+  })
+  test('중복이름의 쿠키값이 있을 경우 첫번째 값을 리턴', () => {
+    const cookie = 'aa=11; aa=22; aa=33'
+    expect(getCookie('aa', cookie)).toEqual('11')
   })
 })
--- test/parse-cookie.test.ts
+++ test/parse-cookie.test.ts
@@ -1,4 +1,4 @@
-import { parseCookie } from "../src";
+import { parseCookie } from '../src'
 
 describe('getCookie', () => {
   test('cookie 문자열을 key:value 객체로 리턴', () => {
@@ -6,7 +6,14 @@ describe('getCookie', () => {
     expect(parseCookie(cookie1).aa).toEqual('11')
     expect(parseCookie(cookie1).bb).toEqual('22')
 
-    const cookie2 = 'confluence.user.history=30364f4c-51bc-4381-9552-38d90ebea36a; atl.xsrf.token=743f15cd6ab0838ef51d3fbfd8015eb09139d48f; ajs_group_id=null; ajs_anonymous_id=%2228303659-e815-4152-9147-bc5b0507ea5b%22; atlassian.xsrf.token=99f15393-229c-44f0-84ec-44068d2f26e3_ef327c58116030e849cf30fb656f8f462e2cc221_lin'
-    expect(parseCookie(cookie2)['atl.xsrf.token']).toEqual('743f15cd6ab0838ef51d3fbfd8015eb09139d48f')
+    const cookie2 =
+      'confluence.user.history=30364f4c-51bc-4381-9552-38d90ebea36a; atl.xsrf.token=743f15cd6ab0838ef51d3fbfd8015eb09139d48f; ajs_group_id=null; ajs_anonymous_id=%2228303659-e815-4152-9147-bc5b0507ea5b%22; atlassian.xsrf.token=99f15393-229c-44f0-84ec-44068d2f26e3_ef327c58116030e849cf30fb656f8f462e2cc221_lin'
+    expect(parseCookie(cookie2)['atl.xsrf.token']).toEqual(
+      '743f15cd6ab0838ef51d3fbfd8015eb09139d48f',
+    )
+  })
+  test('중복 이름의 쿠키가 있을 경우 덮어씀', () => {
+    const cookie3 = 'aa=11; aa=22; aa=33'
+    expect(parseCookie(cookie3).aa).toEqual('33')
   })
 })
