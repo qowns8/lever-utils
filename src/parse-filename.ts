4609fa126bdcca9bf73549ed40101f3a003e09ed
keating <mgsong@madup.com>
2021-08-20 18:31:24 +0900

    new parse-filename.ts

---
+++ src/parse-filename.ts
@@ -0,0 +1,10 @@
+export const parseFilename = (filename: string) => {
+  const idx = filename.lastIndexOf('.')
+  if (idx < 0) {
+    return { name: filename, ext: '' }
+  }
+  return {
+    name: filename.slice(0, idx),
+    ext: filename.slice(idx + 1),
+  }
+}
