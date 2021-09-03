932a953ff25ce3f4f7017f8330e0d7c3298c49cc
keating <mgsong@madup.com>
2021-09-03 12:24:00 +0900

    feat: function item is available

--- src/str-matched.ts
+++ src/str-matched.ts
@@ -1,4 +1,15 @@
-export default (arr: Array<string | RegExp>, target: string): boolean =>
-  arr.some(item =>
-    typeof item === 'string' ? item === target : item.test(target),
-  )
+export default (
+  arr: Array<string | RegExp | ((value: string) => Boolean)>,
+  target: string,
+): boolean =>
+  arr.some(item => {
+    if (typeof item === 'string') {
+      return item === target
+    }
+
+    if (typeof item === 'function') {
+      return item(target)
+    }
+
+    return item.test(target)
+  })
